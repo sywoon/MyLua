4bef8cdcca2de1850c3b5eb03c42ae499bbba55a
sywoon <sywoon@163.com>
2019-03-25 20:18:02 +0800

    rename dir

--- make_lua_cl.bat
+++ make_lua_cl.bat
@@ -4,7 +4,7 @@ call clenv.bat
 
 rmdir bin
 mkdir bin
-cd src
+cd src-lua
 cl /O2 /W3 /c /D_USING_V110_SDK71_ /DLUA_BUILD_AS_DLL *.c  -DLUA_COMPAT_BITLIB
 del lua.obj luac.obj
 link /DLL /out:../bin/lua53.dll *.obj

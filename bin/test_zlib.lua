2f8242360aadca871782bd8541effef76a0ed50a
sywoon <sywoon@163.com>
2019-10-13 10:56:38 +0800

    add zlib test

--- bin/test_zlib.lua
+++ bin/test_zlib.lua
@@ -10,5 +10,12 @@ print("md5 before", md5.sumFile("lua.exe"))
 print(zlib.compressFile("lua.exe", "lua_1.zip"))
 print(zlib.uncompressFile("lua_1.zip", "lua_1.exe"))
 
-
 print("md5 after", md5.sumFile("lua_1.exe"))
+
+
+local str = "hello world"
+local a = zlib.compress(str)
+local s2 = zlib.uncompress(a)
+print(str, a, s2)
+
+

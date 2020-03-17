bcfc0af4348e39c45dacbc2a70f95d7363ae35c5
sywoon <sywoon@163.com>
2020-03-17 17:19:27 +0800

    修复转换工具bug:路径中带有同名文件夹会被删除

--- tools/file_convert/main_path.lua
+++ tools/file_convert/main_path.lua
@@ -9,22 +9,26 @@ local isToUtf8 = (mode == "a2u" or mode == "w2u")
 local files = os.dir(path)
 table.print(files)
 for _, file in ipairs(files) do
-	repeat	
-		local str = io.readFile(file)
-		if isToUtf8 and cs.isutf8(str) then
-			print("already is utf8:", file)
-			break
-		end
-
-		local writepath
+	repeat
+        local writepath
 		if isCover then
 			writepath = file
 		else
-			local subpath = string.gsub(file, path, "")
+			local subpath = string.gsub(file, "^"..path, "")
 			writepath = outpath .. subpath
 			os.mkdir(os.dirname(writepath))
 		end
 		print(file, writepath)
+		
+		local str = io.readFile(file)
+		if isToUtf8 and cs.isutf8(str) then
+			print("already is utf8:", file)
+			if not isCover then
+                --os.copyfile(file, writepath)
+			end
+			break
+		end
+		
 		io.writeFile(writepath, cs[mode](str))
 	until true
 end

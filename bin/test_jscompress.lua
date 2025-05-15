252d448c039babb7f3a74b1c370e7acc23570e46
sywoon <sywoon@163.com>
2025-05-15 20:11:43 +0800

    新增checkJsInstall 验证js命令是否安装

--- bin/test_jscompress.lua
+++ bin/test_jscompress.lua
@@ -1,5 +1,40 @@
 require "sllib_base"
 
+
+function checkInstall2(package_name, showInfo, autoExit)
+    local _log = function (...)
+        if showInfo then
+            print(...)
+        end
+    end
+
+    local check_cmd = "where " .. package_name .. "  >nul 2>&1"
+    
+    local status, msg, code = os.execute(check_cmd)
+    print(check_cmd, status, msg, code)
+    if not status then
+        if showInfo then print("?? " .. package_name .. " 未安装，正在安装...") end
+        do return end
+        local install_cmd = "npm install -g " .. package_name
+        local install_result = os.execute(install_cmd)
+        if install_result ~= 0 then
+            _log("? 安装 " .. package_name .. " 失败，请确保已安装 Node.js 和 npm，并检查网络连接。")
+            if autoExit then os.exit(1) end
+            return false
+        else
+            _log("? " .. package_name .. " 安装成功。")
+            return true
+        end
+    else
+        _log("? " .. package_name .. " 已安装。")
+        return true
+    end
+end
+
+
+checkJsInstall("terser2", true)
+jscompress:checkJsInstall(true)
+
 local from = "test_jscompress.js"
 do
     local to = "test_jscompress_jsmin.js"
